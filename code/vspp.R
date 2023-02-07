6d6f7f0b848f84cf070e58928e86430824f6466f
PChunark <puttipongchunark@gmail.com>
2023-02-07 17:24:12 +0700

    Remove all system system. Just save file into CSV UTF-8 for enable Thai font.

--- code/vspp.R
+++ code/vspp.R
@@ -1,12 +1,8 @@
 library(tidyverse)
 
 
-Sys.setlocale("LC_CTYPE", "Thai")
-options(encoding = "UTF-8")
-locale=locale(encoding="latin1")
+# For Thai font, please save file in CSV UTF-8
 
-read_csv("data/All_Vspp_20221124_PWirat.csv") %>%
-      select("COUNTRY_ZONE", "PROVINCE","EGAT_SUB","PEA_SUB") 
-   
-  str(read_csv("data/All_Vspp_20221124_PWirat.csv"))
-read_csv("data/VSPPTMO_NE.csv")
+read_csv("data/Allonlyutf_Vspp_20221124.csv") 
+
+
