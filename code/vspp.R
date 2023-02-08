72729064f4ff99f7da1761e90b860f219163b23f
PChunark <puttipongchunark@gmail.com>
2023-02-08 10:58:43 +0700

    Select data and plot VSPP

--- code/vspp.R
+++ code/vspp.R
@@ -3,6 +3,20 @@ library(tidyverse)
 
 # For Thai font, please save file in CSV UTF-8
 
-read_csv("data/Allonlyutf_Vspp_20221124.csv") 
+vsppNE<- read_csv("data/vspptmo_adjustNE.csv")%>%
+       select(region = COUNTRY_ZONE,
+              province = PROVINCE,
+              egatSubStation = EGAT_SUB,
+              fuel = FUEL2,
+              contractCapMW = CONTRACTED_CAP_MW) %>%
+       filter(region == "ภาคตะวันออกเฉียงเหนือ")
 
-
+vsppNE %>%
+  ggplot(aes(x = province, y = contractCapMW, group = fuel, color = fuel))+
+  geom_point()+
+  labs(
+    x = "จังหวัด",
+    y = "Contracted Capacity (MW)",
+    title = "กำลังการผลิตตามสัญญาของ VSPP ในภาคอีสาน"
+  )+
+  theme_light()
