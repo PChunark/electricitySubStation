ef08e980de9bc70c5fee999484922d51f61fe234
PChunark <puttipongchunark@gmail.com>
2024-02-22 16:14:07 +0700

    map dataframe

--- code/getdata.R
+++ code/getdata.R
@@ -7,7 +7,8 @@ file_paths <-
     list.files(path = "data/", pattern = "Load profile", full.names = T)
 filenms <-
   list.files(path = "data/", pattern = "Load profile", full.names = F)
-  
+
+names(file_paths) <- c("MEA", "Central R1", "NorthE R2", "South R3", "North R4") 
 
 # substation_profile2019<- 
   file_paths %>% 
@@ -21,14 +22,27 @@ filenms <-
 names(substation_profile2019) <- filenms
 
 
-a<- file_paths[1] %>% 
-  map(function(path){
+a<- file_paths %>% 
+  map_df(function(path){
       read_excel(path) %>% 
+      mutate(total = rowSums(across(where(is.numeric)))) %>% 
       pivot_longer(col = -TIME_LOCAL, names_to = "egatsub", values_to = "kw") %>%  
       arrange(egatsub) %>% 
-      separate(col = egatsub, into = c("sub","voltage"), sep = "/", remove = FALSE)
-                    }
+      separate(col = egatsub, into = c("sub","voltage"), sep = "/", remove = FALSE) 
+                    },
+      .id = "region"
       ) 
-# %>% 
-  # set_names(., nm=filenms) 
-write.xlsx(a[[1]],"data/MEA.xlsx")
+
+b <- a %>% unnest()
+     map_df(.x = file_paths, .id = "region")
+
+
+
+
+
+
+
+file_paths %>% 
+  map(read_excel)
+
+write.xlsx(a,"data/MEA.xlsx")
