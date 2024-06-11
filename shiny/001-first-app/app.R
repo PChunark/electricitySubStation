6a7dd1095ac359fcfab439169106ef12a322b6e7
PChunark <puttipongchunark@gmail.com>
2024-06-11 14:23:40 +0700

    explore shiny apps

---
+++ shiny/001-first-app/app.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(shinythemes)
+
+
+# Define UI
+ui <-
+  fluidPage(theme = shinytheme("cerulean"),
+            navbarPage(
+              # theme = "cerulean", # <--- To use a theme, uncomment this
+              "My first app",
+              tabPanel("Navbar 1",
+                       sidebarPanel(
+                         tags$h3("Input:"),
+                         textInput("txt1", "Given Name:", ""), # txt1 will be sent to the server
+                         textInput("txt2", "Surname:", ""),    # txt2 will be sent to the server
+                         
+                       ), # sidebarPanel
+                       mainPanel(
+                         h1("Header 1"),
+                         
+                         h3("Output 1"),
+                         verbatimTextOutput("txtout"),         # txtout is genrated from the server
+                         
+                         
+                       ) # mainPanel
+                       
+              ), # Navbar 1, tabPanel
+              tabPanel("Navbar 2", "This panel is intentionally left blank"),
+              tabPanel("Navbar 3", "This panel is intentionally left blank")
+            ) # navbarPage
+          ) # fluidPage
+
+# Define server function
+server <- function(input, output){
+  
+  output$txtout <- renderText({
+    paste(input$txt1, input$txt2, sep = " ")
+  })
+} # server
+
+# Create shiny object
+shinyApp(ui = ui, server = server)
